62f36cdc6d2286189e7f5ec57740c124976a2f45
edvalvitor <edval.vitor@gmail.com>
2022-08-09 09:02:08 -0300

    Delete i3blocks directory

--- i3blocks/clima.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-URL='http://www.accuweather.com/pt/br/maceio/31913/weather-forecast/31913'
-SITE="$(curl -s "$URL")"
-
-weather="$(echo "$SITE" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $14 }'| head -1)"
-temp="$(echo "$SITE" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $12"°" }'| head -1)"
-
-echo -e "$temp"
--- i3blocks/ufw.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-status=$(systemctl status ufw | grep Active | awk '{print $2}')
-if [ "$status" = "active" ]; then
-    echo "ON"
-else
-    echo "OFF"
-fi
-
